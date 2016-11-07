abb8e7a4b8eb7a0e11dc641e81a8b195ed62fb23
Christian Mansch <epost@cm-informatics.de>
2016-11-08 00:06:46 +0100

    Autolayout bugs fixed

--- NewCityLife/IssueTableViewController.swift
+++ NewCityLife/IssueTableViewController.swift
@@ -14,12 +14,6 @@ class IssueTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,7 +33,6 @@ class IssueTableViewController: UITableViewController {
         return issues.count
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "issueCell", for: indexPath)
 
@@ -49,79 +42,9 @@ class IssueTableViewController: UITableViewController {
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
-    {
-        print("Row \((indexPath as NSIndexPath).row) was selected")
-        
-       /* if let cell:UITableViewCell = tableView.cellForRowAtIndexPath(indexPath)
-        {
-            print(cell.textLabel!.text!)
-            
-            let vc:ReportsTableViewController = ReportsTableViewController()
-            vc.category = cell.textLabel!.text!
-            
-            
-            
-            //vc.backNavigation(cell.textLabel!.text!)
-            
-            
-        }
-        
-        dismissViewControllerAnimated(true, completion: nil)
-        */
-        
-    }
-    
     // MARK: - BarButton Actions
-    
     @IBAction func cancelAction(_ sender: UIBarButtonItem)
     {
         self.dismiss(animated: true, completion: nil)
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
